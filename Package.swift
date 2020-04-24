63f5cd0ef993134e1b61629f26134205da5512b4
youga <dexterliu1214@gmail.com>
2020-04-24 10:51:49 +0800

    use custom RxBinding

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/dexterliu1214/RxBinding.git", .branch("master")),
         .package(url: "https://github.com/RxSwiftCommunity/RxAnimated.git", from: "0.8.1"),
         .package(url: "https://github.com/RxSwiftCommunity/RxGesture.git", from: "3.0.3"),
         .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", from: "4.0.1"),
