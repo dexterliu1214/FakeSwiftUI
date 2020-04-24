f47bf38621c0ecd1ab809f00f09f6dfabb1946e9
youga <dexterliu1214@gmail.com>
2020-04-24 10:09:24 +0800

    add swiftPM dependency

--- Package.swift
+++ Package.swift
@@ -28,7 +28,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FakeSwiftUI",
-            dependencies: []),
+            dependencies: [
+                "RxSwift",
+                "RxCocoa",
+                "RxAnimated",
+                "RxGesture",
+                "RxDatasouces",
+                "AwaitKit"
+            ]),
         .testTarget(
             name: "FakeSwiftUITests",
             dependencies: ["FakeSwiftUI"]),
