154f085fb5e73d0868ca32d4c55d078891e14358
youga <dexterliu1214@gmail.com>
2020-04-24 10:55:16 +0800

    fix typo

--- Package.swift
+++ Package.swift
@@ -31,6 +31,7 @@ let package = Package(
             name: "FakeSwiftUI",
             dependencies: [
                 "RxSwift",
+                "RxBinding",
                 "RxAnimated",
                 "RxGesture",
                 "RxDataSources",
