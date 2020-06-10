d0001d24eb7904cf31a1724a9157d4e108a97f41
youga <dexterliu1214@gmail.com>
2020-06-10 13:21:47 +0800

    refactor

--- Example/FakeSwiftUI/ImageDemoViewController.swift
+++ Example/FakeSwiftUI/ImageDemoViewController.swift
@@ -31,21 +31,59 @@ class ImageDemoViewController: UIViewController {
         
         view.backgroundColor = .white
         
-        ZStack(
-            Image(Observable.just("https://placeimg.com/640/480/any"))
-                .aspectRatio(contentMode: .scaleAspectFill)
-                .clipShape(Circle())
-                .overlay(Circle().stroke([.green, .yellow], lineWidth: 4))
-                .shadow(color: .black, radius: 10)
-                .blur()
-                .fill()
+//        ZStack(
+//            Image(Observable.just("https://placeimg.com/640/480/any"))
+//                .aspectRatio(contentMode: .scaleAspectFill)
+//                .clipShape(Circle())
+//                .overlay(Circle().stroke([.green, .yellow], lineWidth: 4))
+//                .shadow(color: .black, radius: 10)
+//                .blur()
+//                .fill()
+//        )
+//            .frame(width: 200, height: 200)
+//            .shadow(radius: 10)
+//            .centerX(offset: 0)
+//            .centerY(offset: 0)
+//            .draggable()
+//            .on(view)
+        VStack(alignment: .fill, spacing: 0,
+               HStack(alignment: .center, spacing: 0,
+                      View().background(["38386C", "DEDEDE"].map{ $0.hex!.color }, degree: 90).height(offset: 2),
+                      Text("禮物盒").color(.white).textAlignment(.center).font(20),
+                      View().background(["DEDEDE", "38386C"].map{ $0.hex!.color }, degree: 90).height(offset: 2)
+               )
+                .padding()
+                .distribution(.fillProportionally),
+            Grid(columns:5, items:Observable.just([1])) {(cell:HougongGiftCell, model, row, view) in
+                print(model)
+                return cell
+            }.ratio(0.9)
         )
-            .frame(width: 200, height: 200)
-            .shadow(radius: 10)
-            .centerX(offset: 0)
-            .centerY(offset: 0)
-            .draggable()
-            .on(view)
+            .background(["1D1D46", "6666AD", "202052"].map{ $0.hex!.color }, degree: 30)
+            .fill().on(view)
     }
 }
 
+class HougongGiftCell:UICollectionViewCell
+{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = UIColor.white.withAlphaComponent(0.09)
+        
+        VStack(alignment: .fill, spacing: 0,
+               Image(Observable.just("https://placeimg.com/640/480/any")),
+               HStack(alignment: .center, spacing: 0,
+                      Image(Observable.just("https://placeimg.com/640/480/any")),
+                      Text("100")
+                        .color(.white)
+                )//.padding()
+                .distribution(.fillEqually)
+        )
+        //.padding()
+            .fill().on(contentView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
