1ada88bd10d7c6cf6dc54084ab1360176aaf731b
youga <dexterliu1214@gmail.com>
2020-06-18 09:47:10 +0800

    refactor

--- Sources/FakeSwiftUI/Classes/components/ScalableImage.swift
+++ Sources/FakeSwiftUI/Classes/components/ScalableImage.swift
@@ -16,25 +16,29 @@ import RxBinding
 open class Scrollable:View
 {
     let view = UIScrollView()
+    let contentView = UIView()
     
     public init(_ subview:UIView) {
         super.init()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.append(to: self).fillSuperview()
         
-        self.view.addSubview(subview)
+        view.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
+        contentView.addSubview(subview)
+        subview.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        subview.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        subview.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
+        subview.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
     
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            self.view.contentSize = self.view.subviews.first!.frame.size
-        }
     }
 }
 
